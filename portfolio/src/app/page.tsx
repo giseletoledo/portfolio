116b25f874f5a57b00bdefb80ee78635b84b8ffa
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-07-13 14:31:51 +0000

    adicionando github statistics

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -17,19 +17,45 @@ const Home: React.FC = () => {
         <h1 className={styles.title}>Home</h1>
         <div className={styles.content}>
           <div className={styles.text}>
-            <p>Comecei a trabalhar com web em 2011, no início com jogos educativos, animações e infográficos utilizando a linguagem Actionscript com Adobe Flash, devido a descontinuação do Flash em browsers e a necessidade de uso em dispositivos móveis fiz a transição de tecnologia para Javascript com HTML5.</p> 
-             <p> A partir de 2014, comecei a desenvolver páginas responsivas em times ágeis, consumo de API, desenvolvimento de aplicação backend com Node.js, testes com Selenium e mais recentemente em 2022 testes com Cypress.
-             </p>
-             <p>Participo de comunidades tech e eventos, sempre tentando auxiliar quem está começando.</p>
-            <p>📚 Tenho interesse em desenvolvimento mobile e novas tecnologias, sou uma pessoa que gosta de estar sempre aprendendo novas tecnologias, por isso tenho projetos de diferentes tecnologias.</p>
+            <p>
+              Comecei a trabalhar com web em 2011, no início com jogos educativos, animações e infográficos utilizando a linguagem Actionscript com Adobe Flash, devido a descontinuação do Flash em browsers e a necessidade de uso em dispositivos móveis fiz a transição de tecnologia para Javascript com HTML5.
+            </p>
+            <p>
+              A partir de 2014, comecei a desenvolver páginas responsivas em times ágeis, consumo de API, desenvolvimento de aplicação backend com Node.js, testes com Selenium e mais recentemente em 2022 testes com Cypress.
+            </p>
+            <p>
+              Participo de comunidades tech e eventos, sempre tentando auxiliar quem está começando.
+            </p>
+            <p>
+              📚 Tenho interesse em desenvolvimento mobile e novas tecnologias, sou uma pessoa que gosta de estar sempre aprendendo novas tecnologias, por isso tenho projetos de diferentes tecnologias.
+            </p>
           </div>
           <div className={styles.imageContainer}>
-            <img srcSet="./images/profile.jpeg" alt="Profile" className={styles.image} />
+            <img src="./images/profile.jpeg" alt="Profile" className={styles.image} />
           </div>
         </div>
-        
+        <div className={styles.githubStats}>
+          <h2>GitHub Statistics</h2>
+          <div className={styles.githubStat}>
+            <img
+              src="https://github-readme-stats.vercel.app/api/top-langs/?username=giseletoledo&layout=compact&theme=dark"
+              alt="Top Languages"
+              width="500"
+              height="200"
+            />
+          </div>
+          <div className={styles.githubStat}>
+            <img
+              src="https://github-readme-activity-graph.vercel.app/graph?username=giseletoledo&theme=react-dark"
+              alt="GitHub Activity Graph"
+              width="800"
+              height="300"
+            />
+          </div>
+        </div>
       </motion.div>
     </Layout>
+    
   );
 };
 
