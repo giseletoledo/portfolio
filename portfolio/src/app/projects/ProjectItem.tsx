4e7b81c5aea4dba03c9b6120ea3b2debb8999e6c
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-07-13 03:13:00 +0000

    pagina de projetos

--- portfolio/src/app/projects/ProjectItem.tsx
+++ portfolio/src/app/projects/ProjectItem.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from './projectItem.module.css';
+import Link from 'next/link';
 
 interface ProjectItemProps {
   title: string;
@@ -7,26 +8,38 @@ interface ProjectItemProps {
   features: string;
   technologies: string[];
   imageSrc: string;
+  url: string;
+  size: 'small' | 'large';
 }
 
-const ProjectItem: React.FC<ProjectItemProps> = ({ title, description, features, technologies, imageSrc }) => {
+const ProjectItem: React.FC<ProjectItemProps> = ({ title, description, features, technologies, imageSrc, url, size}) => {
+  const sizes = size === 'large' ? '(max-width: 768px) 100vw, 300px' : '(max-width: 768px) 100vw, 200px';
+
   return (
-    <div className={styles.projectItem}>
+    <div className={`${styles.projectItem} ${styles[size]}`}>
       <h2>{title}</h2>
       <div className={styles.imageContainer}>
         <div className={styles.imageWrapper}>
-          <Image src={imageSrc} alt={title} width={250} height={120} priority />
+          <Image src={imageSrc} alt={title} 
+          fill style={{ objectFit: 'contain', borderRadius: '12px', padding: '0px 10px', backgroundColor:'#282626' }}
+          sizes={sizes}
+          priority />
         </div>
       </div>
       <p>{description}</p>
-      <h4>Funcionalidades</h4>
+      <h4 className={styles.projectItemTitle}>Funcionalidades</h4>
       <p>{features}</p>
-      <h4>Tecnologias Utilizadas</h4>
+      <h4 className={styles.projectItemTitle}>Tecnologias Utilizadas</h4>
       <ul>
         {technologies.map((technology, index) => (
           <li key={index}>{technology}</li>
         ))}
       </ul>
+      <div>
+      <Link href={url} target="_blank" rel="noopener noreferrer">
+{url}
+        </Link>
+      </div>
     </div>
   );
 };
--- portfolio/src/app/projects/page.tsx
+++ portfolio/src/app/projects/page.tsx
@@ -13,6 +13,8 @@ const Projects: React.FC = () => {
       features:'Filtro de busca dos filmes favoritos',
       technologies: ['Angular 17', 'Git', 'GitHub'],
       imageSrc: '/images/movieapi_angular.png',
+      url: 'https://github.com/giseletoledo/ntt-movie',
+      size: 'large',
     },
     {
       title: 'Next.js News Portal',
@@ -20,8 +22,87 @@ const Projects: React.FC = () => {
       features:'Configuração do Next.js para lidar com as requisições do backend. (Saiba mais em Routing: API Routes | Next.js)',
       technologies: ['Next.js 14', 'React 18', 'TypeScript'],
       imageSrc: '/images/newsapi_nextjs.png',
+      url: 'https://github.com/giseletoledo/news-app',
+      size: 'large',
     },
-    // Adicione mais projetos conforme necessário
+    {
+      title: 'Landing page - Nextjs e Django',
+      description: 'Frontend criado com Nextjs na versão 14 com API Route e Django 5.0.6.',
+      features:'Envio de formulário salvando os dados no banco com sqlite',
+      technologies: ['Next.js 14',
+        'React 18',
+        'TypeScript',
+        'Axios',
+        'React-Modal',
+        'Django',
+        'Django Rest Framework'],
+      imageSrc: '/images/fullstack_django.png',
+      url:'https://github.com/giseletoledo/nextjs-django',
+      size: 'large',
+    },
+    {
+      title:'API de Gerenciamento de Filmes',
+      description:'A API de Gerenciamento de Filmes é uma aplicação para gerenciar informações sobre filmes',
+      features:'Filmes: Os filmes são a entidade principal da API e possuem diversos atributos para descrever suas características.Usuários: Usuários podem adicionar os filmes favoritos',
+      technologies: ['Java 17','JPA','Banco de dados H2','Postman'],
+      imageSrc:'/images/apimovies_java.png',
+      url: 'https://github.com/giseletoledo/academianttv2',
+      size: 'large',
+    },
+    {
+      title:'DIO - Trilha .NET - API e Entity Framework',
+      description:'API e Entity Framework, da trilha .NET da DIO.',
+      features: 'Um sistema gerenciador de tarefas, onde você poderá cadastrar uma lista de tarefas que permitirá organizar melhor a sua rotina.',
+      technologies: ['.NET 8', 'ASP.NET Core','Entity Framework Core','SQL Server','Swagger'],
+      imageSrc:'/images/apitarefas_csharp.png',
+      url: 'https://github.com/giseletoledo/trilha-net-api-desafio',
+      size: 'large',
+    },
+    {
+      title:'Projeto: Catálogo de Livros com SQLite',
+      description:'Este projeto implementa um sistema de catálogo de livros utilizando a biblioteca SQLite em Python. Projeto final do curso da IBM',
+      features: 'O sistema permite cadastrar livros, buscar livros pelo título ou autor, verificar a disponibilidade de exemplares e realizar o empréstimo de livros.',
+      technologies: ['Python','SQLite','Jupyter Notebook'],
+      imageSrc:'/images/cadastro_python.png',
+      url: 'https://github.com/giseletoledo/pythonSql',
+      size: 'large',
+    },
+    {
+      title:'[Backend] Desafio Autodidata - Potência Tech',
+      description:'Esta é uma API Node.js para gerenciar registros e logins de usuários.',
+      features: 'Criptografia de senha e autenticação',
+      technologies: ['Node.js', 'Express.js','bcrypt'],
+      imageSrc:'/images/autenticacao_node.png',
+      url: 'https://github.com/giseletoledo/user-auth',
+      size: 'large',
+    },
+    {
+      title:'Chef Delivery App',
+      description:'Projeto utilizando Swift UI',
+      features: 'Lista um cardápio, com animação na splash screen e calculo de preços no carrinho.',
+      technologies: ['Swift', 'SwiftUI', 'XCode'],
+      imageSrc:'/images/ChefDelivery_iPhone.gif',
+      url: 'https://github.com/giseletoledo/chef-delivery-app',
+      size: 'small',
+    },
+    {
+      title:'Expenses App',
+      description:'Projeto de um app que controla gastos. Desafio do curso da MJV de criar um app com os conceitos ensinados durante o curso no prazo de 5 dias',
+      features: 'Controle de gastos divididos em categorias',
+      technologies: ['Flutter', 'Provider'],
+      imageSrc:'/images/expensesapp_flutter.png',
+      url: 'https://github.com/giseletoledo/expenses-app',
+      size: 'small',
+    },
+    {
+      title:'HQ App',
+      description:'Projeto para listar dados da API',
+      features: 'Listar dados da API da Marvel(http://marvel.com) e salva no banco de dados',
+      technologies: ['Android', 'Viewmodel', 'RecyclerView', 'Databinding'],
+      imageSrc:'/images/apimovies_android.png',
+      url: 'https://github.com/giseletoledo/hq-comic-app',
+      size: 'small',
+    }
   ];
 
   return (
@@ -49,6 +130,8 @@ const Projects: React.FC = () => {
               features={project.features}
               technologies={project.technologies}
               imageSrc={project.imageSrc}
+              url={project.url}
+              size={project.size === 'large' || project.size === 'small' ? project.size : 'small'}
             />
           </motion.div>
         ))}
